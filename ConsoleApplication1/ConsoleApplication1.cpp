040dba6d8273a18e8e663a9accd17abbd488ec62
Wenk21 <wenk20@yandex.ru>
2024-06-11 14:27:18 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main() {
+	std::cout << "Введите первое число";
+	std::cin >> firstNumber;
+
+	std::cout << "Введите второе число";
+	std::cin >> secondNumber;
+
+	int sum = firstNumber + secondNumber;
+
+	std::cout << 
+
+
+
+}
